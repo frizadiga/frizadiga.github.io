6c3f006bc85093ebc21bfac2af208807e66a2c9b
frizadiga <frizadiga@gmail.com>
2018-01-03 05:12:38 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'frizadiga site', name: 'frizadiga site', content: 'frizadiga site' }
+      { hid: 'description', name: 'description', content: 'frizadiga site' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/hiu-favicon.png' }
