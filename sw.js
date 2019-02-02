2f278f8ed19a729e823e67f3f998d3f19e89e15f
frizadiga <frizadiga@gmail.com>
2019-02-02 14:53:05 +0000

    Automated deployment to GitHub Pages: 96f402fc0ec2d594aad4a60fb49df4c937e77938

--- sw.js
+++ sw.js
@@ -2,24 +2,24 @@ importScripts('/./workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/./1b135404bf2ec2211e26.js",
-    "revision": "2e74b039adef0a8cc9da8431781a767f"
+    "url": "/./1d492396db74eae72eb8.js",
+    "revision": "6631f9ef3cf06dae5f6362be75791232"
   },
   {
-    "url": "/./66e8d2ae0141e79cb718.js",
-    "revision": "363fec5f17171b70d034d9dfe9be71eb"
+    "url": "/./2eb83e1c73227c26e760.js",
+    "revision": "19bdeb4c816c7b83fb9ea169cccbe3e3"
   },
   {
-    "url": "/./ad651ca057cb4e6fd562.js",
-    "revision": "37c5a5867c478d97f8a0aa32ce90a445"
+    "url": "/./5590a6830e528ac27964.js",
+    "revision": "bdeb56c7020b744cfe81118854906675"
   },
   {
-    "url": "/./f372619f7513b5a40394.js",
-    "revision": "6bf3f235d81da3d626fb965d69ab95f1"
+    "url": "/./68634194c1929a052a78.js",
+    "revision": "e034be7d11152565468d53d891b66732"
   },
   {
-    "url": "/./fb1cf274c85a792e607a.js",
-    "revision": "e0dd64a7789e8798edc9028959b73049"
+    "url": "/./ce567d442013a56ac5de.js",
+    "revision": "0c19ced95d55aa4d005ee8149af2de3b"
   }
 ], {
   "cacheId": "frizadiga-site",
