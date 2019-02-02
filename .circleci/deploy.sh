8521e6f7ccdecd4a4ee63b0d44414bcc1774529c
frizadiga <frizadiga@gmail.com>
2019-02-02 21:04:55 +0700

    add continuous deployment

---
+++ .circleci/deploy.sh
@@ -0,0 +1,27 @@
+set -o nounset
+set -o errexit
+set -o xtrace
+
+if [$CIRCLE_BRANCH == $SOURCE_BRANCH]; then
+  git config --global user.name $GH_NAME
+  git config --global user.email $GH_EMAIL
+
+  git clone $CIRCLE_REPOSITORY_URL deploy
+  cd deploy
+
+  git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+  git rm -rf .
+  cd ..
+
+  npm run build
+
+  cp -a dist/. deploy/.
+
+  mkdir -p deploy/.circleci
+  cp -a .circleci/. deploy/.circleci/.
+  cd deploy
+
+  git add .
+  git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}" --allow-empty
+  git push origin $TARGET_BRANCH
+fi
