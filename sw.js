d71e38466f53b3faef0fd0ae8175095994e8a7a7
frizadiga <frizadiga@gmail.com>
2019-03-03 09:53:52 +0000

    Automated deployment to GitHub Pages: c6f5b2e27d3fd73170d63526a4cc54493e948d44

--- sw.js
+++ sw.js
@@ -1,35 +1,32 @@
-importScripts('/./workbox.4c4f5ca6.js')
+importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn@4.0.0-0/workbox/workbox-sw.js')
 
-workbox.precaching.precacheAndRoute([
-  {
-    "url": "/./1d492396db74eae72eb8.js",
-    "revision": "6631f9ef3cf06dae5f6362be75791232"
-  },
-  {
-    "url": "/./2eb83e1c73227c26e760.js",
-    "revision": "19bdeb4c816c7b83fb9ea169cccbe3e3"
-  },
-  {
-    "url": "/./9ac7bb07457a8a89a1e4.js",
-    "revision": "e24529435bd95ee46492641305b6bf05"
-  },
-  {
-    "url": "/./b21e395fb499f998958e.js",
-    "revision": "67ca57959b385d8261bf7c2110e7b6b5"
-  },
-  {
-    "url": "/./d32b55516cbb2f1c2475.js",
-    "revision": "9eb4ed0df1af4190acccbc56918af744"
-  }
-], {
-  "cacheId": "frizadiga-site",
-  "directoryIndex": "/",
-  "cleanUrls": false
+// --------------------------------------------------
+// Configure
+// --------------------------------------------------
+
+// Set workbox config
+workbox.setConfig({
+  "debug": false
 })
 
-workbox.clientsClaim()
-workbox.skipWaiting()
+// Start controlling any existing clients as soon as it activates
+workbox.core.clientsClaim()
 
-workbox.routing.registerRoute(new RegExp('/./.*'), workbox.strategies.cacheFirst({}), 'GET')
+// Skip over the SW waiting lifecycle stage
+workbox.core.skipWaiting()
 
-workbox.routing.registerRoute(new RegExp('/.*'), workbox.strategies.networkFirst({}), 'GET')
+workbox.precaching.cleanupOutdatedCaches()
+
+// --------------------------------------------------
+// Precaches
+// --------------------------------------------------
+
+// Precache assets
+
+// --------------------------------------------------
+// Runtime Caching
+// --------------------------------------------------
+
+// Register route handlers for runtimeCaching
+workbox.routing.registerRoute(new RegExp('/(?!.*(__webpack_hmr|hot-update))'), new workbox.strategies.CacheFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/(?!.*(__webpack_hmr|hot-update))'), new workbox.strategies.NetworkFirst ({}), 'GET')
